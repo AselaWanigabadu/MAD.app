c38aacbcdb2b19c59cbeace39ea434ad251b1e67
Asela Wanigabadu <159133313+AselaWanigabadu@users.noreply.github.com>
2024-05-11 18:51:02 +0530

    AddNote activity edit

--- app/src/main/java/com/example/madapp2/AddNoteActivity.kt
+++ app/src/main/java/com/example/madapp2/AddNoteActivity.kt
@@ -2,10 +2,30 @@ package com.example.madapp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.madapp2.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db:NoteDataBaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_note)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db= NoteDataBaseHelper(this)
+
+        binding.saveButton.setOnClickListener{
+            val title=binding.titleEditTextt.text.toString()
+            val content=binding.contentEditText.text.toString()
+            val note=Note(0,title,content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this,"Note Saved",Toast.LENGTH_SHORT).show()
+
+
+        }
+
     }
 }
--- app/src/main/java/com/example/madapp2/MainActivity.kt
+++ app/src/main/java/com/example/madapp2/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.madapp2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.madapp2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener{
+            val intent=Intent(this,AddNoteActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
