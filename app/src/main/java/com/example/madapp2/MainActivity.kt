d00d8b2ede861f22bb9c391d30d666fb6c665783
Asela Wanigabadu <159133313+AselaWanigabadu@users.noreply.github.com>
2024-05-12 13:00:01 +0530

    Recycler View

--- app/src/main/java/com/example/madapp2/MainActivity.kt
+++ app/src/main/java/com/example/madapp2/MainActivity.kt
@@ -3,21 +3,35 @@ package com.example.madapp2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.madapp2.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db: NoteDataBaseHelper
+    private lateinit var notesAdapter: NotesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.addButton.setOnClickListener{
-            val intent=Intent(this,AddNoteActivity::class.java)
+        db = NoteDataBaseHelper(this)
+        notesAdapter = NotesAdapter(db.getAllNotes(), this)
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter = notesAdapter
+
+        binding.addButton.setOnClickListener {
+            val intent = Intent(this, AddNoteActivity::class.java)
             startActivity(intent)
         }
+    }
 
-
+    override fun onResume() {
+        super.onResume()
+        notesAdapter.refreshData(db.getAllNotes())
     }
 }
